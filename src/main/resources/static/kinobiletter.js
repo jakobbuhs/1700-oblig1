434bb519f2ef376119db5e2d32753cc66187274c
jakobbuhs <143163665+jakobbuhs@users.noreply.github.com>
2024-02-13 07:12:12 +0100

    endringer

--- src/main/resources/static/kinobiletter.js
+++ src/main/resources/static/kinobiletter.js
@@ -2,7 +2,10 @@
 let liste = [];
 function valideringEpost() {
     let epost = document.getElementById("epost").value;
-    if(epost.match(/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/)){
+    if(epost===""){
+        document.getElementById("feilmeldingEpost").innerHTML = "Du må huske å skrive inn eposten din!";
+        return false;}
+    else if(epost.match(/^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/)){
         document.getElementById("feilmeldingEpost").innerHTML = "";
         return true;
     }else {
@@ -12,32 +15,55 @@ function valideringEpost() {
 }
 function valideringFornavn(){
     let fornavn = document.getElementById("fornavn").value;
-    if(isNaN(fornavn)){
-        document.getElementById("feilmeldingFornavn").innerHTML = "";
-        return true;
-    }else{
+    if(fornavn===""){
+        document.getElementById("feilmeldingFornavn").innerHTML = "Du må huske å skrive inn fornavnet ditt!";
+        return false;
+    }
+    else if(/\d/.test(fornavn)
+    ){
         document.getElementById("feilmeldingFornavn").innerHTML = "Du har tall i fornavnet ditt.";
         return false;
+    }else{
+
+        document.getElementById("feilmeldingFornavn").innerHTML = "";
+        return true;
     }
 }
 function valideringEtternavn(){
     let etternavn = document.getElementById("fornavn").value;
-    if(isNaN(etternavn)){
-        document.getElementById("feilmeldingEtternavn").innerHTML = "";
-        return true;
-    }else{
+    if(etternavn===""){
+        document.getElementById("feilmeldingEtternavn").innerHTML = "Du må huske å skrive inn etternavnet ditt!";
+        return false;}
+    if(/\d/.test(etternavn)){
         document.getElementById("feilmeldingEtternavn").innerHTML = "Du har tall i etternavnet ditt.";
         return false;
+
+    }else{
+        document.getElementById("feilmeldingEtternavn").innerHTML = "";
+        return true;
     }
 }
 function valideringNummer(){
     let nummer = Number(document.getElementById("nummer").value);
+    if(nummer===0){
+        document.getElementById("feilmeldingNummer").innerHTML = "Har du kanskje glemt å skrive inn nummeret ditt?!";
+        return false;}
     if(10000000 < nummer && nummer < 99999999){
         document.getElementById("feilmeldingNummer").innerHTML = ""
         return true;
     }else {
         document.getElementById("feilmeldingNummer").innerHTML = "Du har skrevet inn et ugyldig telefonnummer.";
         return false;
+    }
+}
+function valideringBillett(){
+    let billett = Number(document.getElementById("antall"));
+    if(billett===0){
+        document.getElementById("feilmeldingAntall").innerHTML = "Du må huske å skrive inn hvor mange dere skal være!";
+        return false;}
+    else{
+        document.getElementById("feilmeldingAntall").innerHTML = "";
+        return true
     }
 }
 function billettKjop(){
@@ -62,7 +88,7 @@ function billettKjop(){
         liste.push(pers)
         let ut = "";
         for (let i of liste){
-            ut += i.fornavn_dict + " " + i.etternavn_dict + " Skal se: " + i.film_dict  + " og har med seg: " + i.antall_dict + " personer,";
+            ut += i.fornavn_dict + " " + i.etternavn_dict + " Skal se: " + i.film_dict  + " og er totalt " + i.antall_dict + " personer,";
             ut += " Kontakt info: epost: " + i.epost_dict + " tlf: " + i.nummer_dict + "</br>";
         document.getElementById("billetter").innerHTML = ut;
         console.log(1)}
